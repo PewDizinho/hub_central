4a2e29f129892929714efa75524aff8a39d786e3
PewDizinho <konopkspaulo046@gmail.com>
2022-12-03 21:51:17 -0300

    commit

--- lib/config/handler.dart
+++ lib/config/handler.dart
@@ -1,7 +1,6 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
-
 import '../pages/home.dart';
 
 class Handler {
@@ -44,7 +43,6 @@ class Handler {
                   ),
                   ModalRoute.withName('/'));
             }
-
             break;
           case 1:
             if (button2 != null) {
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -16,61 +16,61 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        //
-        backgroundColor: Preferences().getBackgroundColor(),
-        bottomNavigationBar: Handler().getNavBar(null, null, null, context),
-        body: Center(
-          child: Column(
-            children: [
-              Handler().getMargin(null),
-              Handler().createText("Bem-Vindo Paulo!", 50, null, null),
-              Row(
-                children: [
-                  SizedBox(
-                    width: 50.w,
-                    height: 50.h,
-                    child: Container(
-                      margin: const EdgeInsets.all(15.0),
-                      padding: const EdgeInsets.all(3.0),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.white,
-                        ),
+      backgroundColor: Preferences().getBackgroundColor(),
+      bottomNavigationBar: Handler().getNavBar(null, null, null, context),
+      body: Center(
+        child: Column(
+          children: [
+            Handler().getMargin(null),
+            Handler().createText("Bem-Vindo Paulo!", 50, null, null),
+            Row(
+              children: [
+                SizedBox(
+                  width: 50.w,
+                  height: 50.h,
+                  child: Container(
+                    margin: const EdgeInsets.all(15.0),
+                    padding: const EdgeInsets.all(3.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.white,
                       ),
                     ),
                   ),
-                  SizedBox(
-                    width: 50.w,
-                    height: 50.h,
-                    child: Container(
-                      margin: const EdgeInsets.all(15.0),
-                      padding: const EdgeInsets.all(3.0),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              const Spacer(
-                flex: 1,
-              ),
-              CircleAvatar(
-                radius: 30.sp,
-                backgroundColor: Colors.white,
-                child: IconButton(
-                  icon: const Icon(Icons.add),
-                  iconSize: 55.sp,
-                  onPressed: () {},
                 ),
+                SizedBox(
+                  width: 50.w,
+                  height: 50.h,
+                  child: Container(
+                    margin: const EdgeInsets.all(15.0),
+                    padding: const EdgeInsets.all(3.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const Spacer(
+              flex: 1,
+            ),
+            CircleAvatar(
+              radius: 30.sp,
+              backgroundColor: Colors.white,
+              child: IconButton(
+                icon: const Icon(Icons.add),
+                iconSize: 55.sp,
+                onPressed: () {},
               ),
-              const Spacer(
-                flex: 2,
-              ),
-            ],
-          ),
-        ));
+            ),
+            const Spacer(
+              flex: 2,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
